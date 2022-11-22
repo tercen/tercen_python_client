e94c1de111af8b7a0e545527fd52f2d037b6a016
Thiago Monteiro <tsmonteiro@gmail.com>
2022-11-22 14:52:57 -0300

    Split tercen context in dev and production

--- tercen/client/context.py
+++ tercen/client/context.py
@@ -1,25 +1,197 @@
 import pandas as pd
-import multiprocessing
+import multiprocessing, sys
 
 from tercen.model.base import OperatorResult, FileDocument, ComputationTask, InitState, RunComputationTask, FailedState
+from tercen.client.factory import TercenClient
 from tercen.util import helper_functions as utl
 from tercen.http.HttpClientService import encodeTSON
 
 
-
-
 class TercenContext:
-    # FIXME client should be obtained from elsewhere
-    def __init__(self, client, session, workflowId, stepId, authToken = None, taskId = None):
-        self.client = client
-        self.workflowId = workflowId
-        self.session = session
+    def __init__(self, workflowId = None, stepId = None, username = 'test', password = 'test',
+     authToken = None, taskId = None, serviceUri = "http://127.0.0.1:5402/"):
+        args = self.parse_args()
 
-        self.cubeQuery = client.workflowService.getCubeQuery(workflowId, stepId)
+        if not args["taskId"] is None:
+            taskId = args["taskId"]
 
-        self.schema = client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.qtHash] )[0]
-        self.cschema = client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.columnHash] )[0]
-        self.rschema = client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.rowHash] )[0]
+        if not args["token"] is None:
+            authToken = args["token"]
+
+        if not args["serviceUri"] is None:
+            serviceUri = args["serviceUri"]
+
+        if taskId == None:
+            self.context = OperatorContextDev(workflowId, stepId, 
+                        authToken, username, password, serviceUri)
+        else:
+            self.context = OperatorContext( authToken, username, password, taskId , serviceUri  )
+        
+
+    def parse_args(self) -> dict:
+        taskId = None
+        serviceUri = None
+        token = None
+
+        for arg in sys.argv:
+            if str.startswith(arg, '--') and str.find(arg, '=') > 0:
+                argParts = str.split(arg, '=')
+                argName = str.removeprefix(argParts[0], '--')
+
+                if argName == 'taskId':
+                    taskId = argParts[1]
+                
+                if argName == 'serviceUri':
+                    serviceUri = argParts[1]
+                
+                if argName == 'token':
+                    token = argParts[1]
+
+        return {'taskId':taskId, 
+                'serviceUri':serviceUri, 
+                'token':token}
+
+    def select(self, names=[], offset=0, nr=None) -> pd.DataFrame:
+        if not nr is None and nr < 0:
+            nr = None
+
+        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
+            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.context.schema.columns ])
+            names = [ c.name for c in utl.get_from_idx_list( self.context.schema.columns, where) ]
+
+        if( self.context.isPairwise ):
+            res = self.context.client.tableSchemaService.selectPairwise(  self.context.schema.id, names, offset, nr)
+        else:
+            res = self.context.client.tableSchemaService.select(  self.context.schema.id, names, offset, nr)
+
+        df = pd.DataFrame()
+
+        for c in res.columns:
+            df[c.name] = c.values
+
+        return df
+
+    def cselect(self, names=[], offset=0, nr=None) -> pd.DataFrame:
+        if not nr is None and nr < 0:
+            nr = None
+
+        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
+            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.context.cschema.columns ])
+            names = [ c.name for c in utl.get_from_idx_list( self.context.cschema.columns, where) ]
+
+        res = self.context.client.tableSchemaService.select(  self.context.cschema.id, names, offset, nr)
+
+        df = pd.DataFrame()
+
+        for c in res.columns:
+            df[c.name] = c.values
+
+        
+        return df
+
+    def rselect(self, names=[], offset=0, nr=None) -> pd.DataFrame:
+        if not nr is None and nr < 0:
+            nr = None
+
+        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
+            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.context.rschema.columns ])
+            names = [ c.name for c in utl.get_from_idx_list( self.context.rschema.columns, where) ]
+
+        res = self.context.client.tableSchemaService.select(  self.context.rschema.id, names, offset, nr)
+
+        df = pd.DataFrame()
+
+        for c in res.columns:
+            df[c.name] = c.values
+
+        
+        return df
+
+    def available_cores(self) -> int:
+        return multiprocessing.cpu_count()
+
+    def add_namespace( self, df ) -> pd.DataFrame:
+        colnames = df.columns
+
+        def addPrefix(x, ns):
+            if not str.startswith(x, "."):
+                return ''.join((ns, '.', x))
+            else:
+                return x
+
+        df.columns = [ addPrefix(c, self.context.namespace) for c in colnames ]
+
+        return df
+
+    def save( self, df ) -> None:
+        if issubclass(df.__class__, OperatorResult):
+            result = df
+        else:
+            result = OperatorResult()
+
+            if isinstance(df, list):
+                result.tables = [ utl.pandas_to_table(t) for t in df ]
+            else:
+                result.tables = [utl.pandas_to_table(df)]
+        
+        
+        resultBytes = encodeTSON( result.toJson() ) 
+
+        fileDoc = FileDocument()
+        fileDoc.name = 'result'
+        workflow = self.context.client.workflowService.get( self.context.workflowId )
+        fileDoc.projectId = workflow.projectId
+        fileDoc.acl.owner = workflow.acl.owner
+        fileDoc.metadata.contentType = 'application/octet-stream'
+
+        fileDoc = self.context.client.fileService.upload( fileDoc, resultBytes )
+
+        task = None 
+        if task is None:
+            print("task is null, create a task")
+            if self.context.session.serverVersion is None:
+                task = ComputationTask()
+            else:
+                task = RunComputationTask()
+
+            task.state = InitState()
+            task.owner = workflow.acl.owner
+            task.projectId = workflow.projectId
+            task.query = self.context.cubeQuery
+            task.fileResultId = fileDoc.id
+            task = self.context.client.taskService.create(task)
+        else:
+            task.fileResultId = fileDoc.id
+            rev = self.context.client.taskService.update(task)
+            task.rev = rev
+
+        self.context.client.taskService.runTask( task.id )
+        task = self.context.client.taskService.waitDone( task.id )
+
+        if issubclass(task.state.__class__, FailedState):
+            raise task.state.reason
+
+class OperatorContext(TercenContext):
+    def __init__(self, authToken, username, password, serviceUri, taskId):
+        self.client = TercenClient(serviceUri)
+        # self$userService$client$token = self$session$token$token
+        if authToken is None:
+            self.session = self.client.userService.connect(username, password)
+            self.client.userService.tercenClient.token = self.session.token.token
+        else:
+            self.client.userService.tercenClient.token = authToken
+
+        self.client.userService.client.token = self.session.token.token
+
+        self.task = self.client.taskService.get( taskId )
+
+
+        self.cubeQuery = self.task.query
+
+
+        self.schema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.qtHash] )[0]
+        self.cschema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.columnHash] )[0]
+        self.rschema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.rowHash] )[0]
 
         self.names = [col.name for col in self.schema.columns] 
         self.cnames = [col.name for col in self.cschema.columns] 
@@ -43,85 +215,11 @@ class TercenContext:
 
         self.isPairwise = len(set(self.cnames).intersection( set(self.rnames) )) > 0
 
-        if not taskId is None:
-            self.task = self.client.taskService.get( taskId )
-        else:
-            self.task = None
+        
 
         self.namespace = self.cubeQuery.operatorSettings.namespace
 
-    def select(self, names=[], offset=0, nr=None) -> pd.DataFrame:
-        if not nr is None and nr < 0:
-            nr = None
-
-        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
-            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.schema.columns ])
-            names = [ c.name for c in utl.get_from_idx_list( self.schema.columns, where) ]
-
-        if( self.isPairwise ):
-            res = self.client.tableSchemaService.selectPairwise(  self.schema.id, names, offset, nr)
-        else:
-            res = self.client.tableSchemaService.select(  self.schema.id, names, offset, nr)
-
-        df = pd.DataFrame()
-
-        for c in res.columns:
-            df[c.name] = c.values
-
-        
-        return df
-
-    def cselect(self, names=[], offset=0, nr=None) -> pd.DataFrame:
-        if not nr is None and nr < 0:
-            nr = None
-
-        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
-            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.cschema.columns ])
-            names = [ c.name for c in utl.get_from_idx_list( self.cschema.columns, where) ]
-
-        res = self.client.tableSchemaService.select(  self.cschema.id, names, offset, nr)
-
-        df = pd.DataFrame()
-
-        for c in res.columns:
-            df[c.name] = c.values
-
-        
-        return df
-
-    def rselect(self, names=[], offset=0, nr=None) -> pd.DataFrame:
-        if not nr is None and nr < 0:
-            nr = None
-
-        if( names is None or len(names) == 0 or (len(names) == 1 and names[0] == '' )):
-            where = utl.logical_index([ c.type != 'uint64' and c.type != 'int64' for c in self.rschema.columns ])
-            names = [ c.name for c in utl.get_from_idx_list( self.rschema.columns, where) ]
-
-        res = self.client.tableSchemaService.select(  self.rschema.id, names, offset, nr)
-
-        df = pd.DataFrame()
-
-        for c in res.columns:
-            df[c.name] = c.values
-
-        
-        return df
-
-    def available_cores(self) -> int:
-        return multiprocessing.cpu_count()
-
-    def add_namespace( self, df ) -> pd.DataFrame:
-        colnames = df.columns
-
-        def addPrefix(x, ns):
-            if not str.startswith(x, "."):
-                return ''.join((ns, '.', x))
-            else:
-                return x
-        df.columns = [ addPrefix(c, self.namespace) for c in colnames ]
-
-        return df
-    def save( self, df ) -> None:
+    def save(self, df ) -> None:
         if issubclass(df.__class__, OperatorResult):
             result = df
         else:
@@ -135,39 +233,72 @@ class TercenContext:
         
         resultBytes = encodeTSON( result.toJson() ) 
 
-        fileDoc = FileDocument()
-        fileDoc.name = 'result'
-        workflow = self.client.workflowService.get( self.workflowId )
-        fileDoc.projectId = workflow.projectId
-        fileDoc.acl.owner = workflow.acl.owner
-        fileDoc.metadata.contentType = 'application/octet-stream'
 
-        fileDoc = self.client.fileService.upload( fileDoc, resultBytes )
 
-        task = None 
-        if task is None:
-            print("task is null, create a task")
-            if self.session.serverVersion is None:
-                task = ComputationTask()
-            else:
-                task = RunComputationTask()
+        if( len(self.task.fileResultId) == 0 ):
+            #Webapp scenario
+            fileDoc = FileDocument()
+            fileDoc.name = 'result'
 
-            task.state = InitState()
-            task.owner = workflow.acl.owner
-            task.projectId = workflow.projectId
-            task.query = self.cubeQuery
-            task.fileResultId = fileDoc.id
-            task = self.client.taskService.create(task)
+            fileDoc.projectId = self.context.task.projectId
+            fileDoc.acl.owner = self.context.task.acl.owner
+            fileDoc.metadata.contentType = 'application/octet-stream'
+
+            fileDoc = self.context.client.fileService.upload( fileDoc, resultBytes )
+
+            self.task.fileResultId = fileDoc.id
+            self.task.rev = self.client.taskService.update(self.task)
+            self.task = self.client.taskService.waitDone(self.task.id)
+
+            if issubclass(self.task.state, FailedState):
+                raise self.task.state.reason
+
         else:
-            task.fileResultId = fileDoc.id
-            rev = self.client.taskService.update(task)
-            task.rev = rev
-
-        self.client.taskService.runTask( task.id )
-        task = self.client.taskService.waitDone( task.id )
-
-        if issubclass(task.state.__class__, FailedState):
-            raise task.state.reason
+            fileDoc = self.client.fileService.get(self.task.fileResultId)
+            self.client.fileService.upload(fileDoc, bytes)
 
         
 
+class OperatorContextDev(TercenContext):
+    def __init__(self, workflowId = None, stepId = None, authToken = None, username = None, password = None,
+                         serviceUri  = None):
+        self.client = TercenClient(serviceUri)
+        if authToken is None:
+            self.session = self.client.userService.connect(username, password)
+            self.client.userService.tercenClient.token = self.session.token.token
+        else:
+            self.client.userService.tercenClient.token = authToken
+
+        self.workflowId = workflowId
+
+        self.cubeQuery = self.client.workflowService.getCubeQuery(workflowId, stepId)
+
+        self.schema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.qtHash] )[0]
+        self.cschema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.columnHash] )[0]
+        self.rschema = self.client.tableSchemaService.findByQueryHash( keys=[self.cubeQuery.rowHash] )[0]
+
+        self.names = [col.name for col in self.schema.columns] 
+        self.cnames = [col.name for col in self.cschema.columns] 
+        self.rnames = [col.name for col in self.rschema.columns] 
+
+        axisQueries = self.cubeQuery.axisQueries
+
+        self.colors = [c.name for q in axisQueries for c in q.colors]
+        self.labels = [l.name for q in axisQueries for l in q.labels]
+        self.errors = [l.name for q in axisQueries for l in q.errors]
+        self.chartType = [q.chartType for q in axisQueries ]
+        self.pointSizes = [q.pointSize for q in axisQueries ]
+
+        
+        self.yAxis = utl.unique_and_nonempty( [q.yAxis.name for q in axisQueries])
+        self.xAxis = utl.unique_and_nonempty( [q.xAxis.name for q in axisQueries])
+
+
+        self.hasXAxis = any([ col.name == ".x" for col in self.schema.columns ])
+        self.hasNumericXAxis = any([ col.name == ".x" and col.type == "double" for col in self.schema.columns ])
+
+        self.isPairwise = len(set(self.cnames).intersection( set(self.rnames) )) > 0
+
+        self.task = None
+
+        self.namespace = self.cubeQuery.operatorSettings.namespace
--- tests/test_context_select.py
+++ tests/test_context_select.py
@@ -11,9 +11,11 @@ from tercen.client import context as ctx
 
 class TestTercen(unittest.TestCase):
     def setUp(self):
-        self.client = TercenClient("http://127.0.0.1:5402/")
-        self.session = self.client.userService.connect('test', 'test')
-        
+        # self.client = TercenClient("http://127.0.0.1:5402/")
+        # self.session = self.client.userService.connect('test', 'test')
+        self.context = ctx.TercenContext(
+                        stepId="2ca54ff5-5b7f-44e9-870b-48facabc41ae",
+                        workflowId="9b611b90f412969d6f617f559f005bc6")
 
         
 
@@ -23,13 +25,14 @@ class TestTercen(unittest.TestCase):
         # http://127.0.0.1:5402/test/w/9b611b90f412969d6f617f559f005bc6/ds/2ca54ff5-5b7f-44e9-870b-48facabc41ae
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_1.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
+        # tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
+        #             "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
 
         selNames = ['.y']
 
+
         targetDf = targetDf[selNames]
-        resDf = tercenCtx.select( selNames )
+        resDf = self.context.select( selNames )
         
       
         assert( not resDf is None )
@@ -40,13 +43,10 @@ class TestTercen(unittest.TestCase):
         # http://127.0.0.1:5402/test/w/9b611b90f412969d6f617f559f005bc6/ds/2ca54ff5-5b7f-44e9-870b-48facabc41ae
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_1.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['.y', 'Facility.Type', 'Facility.Name' ]
 
         targetDf = targetDf[selNames]
-        resDf = tercenCtx.select( selNames )
+        resDf = self.context.select( selNames )
         
         assert( not resDf is None )
         assert(resDf.shape == targetDf.shape)
@@ -58,13 +58,10 @@ class TestTercen(unittest.TestCase):
         # http://127.0.0.1:5402/test/w/9b611b90f412969d6f617f559f005bc6/ds/2ca54ff5-5b7f-44e9-870b-48facabc41ae
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_1.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['']
 
         targetDf = targetDf.drop(".cri", axis=1) # int64
-        resDf = tercenCtx.select( selNames )
+        resDf = self.context.select( selNames )
         
       
         assert( not resDf is None )
@@ -79,12 +76,9 @@ class TestTercen(unittest.TestCase):
     def test_select_all_columns(self):
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_2.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['']
 
-        resDf = tercenCtx.cselect( selNames )
+        resDf = self.context.cselect( selNames )
 
         assert( not resDf is None )
         assert(resDf.shape == targetDf.shape)
@@ -97,12 +91,9 @@ class TestTercen(unittest.TestCase):
     def test_select_columns(self):
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_2.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['Rating.Imaging']
         targetDf = targetDf[selNames]
-        resDf = tercenCtx.cselect( selNames )
+        resDf = self.context.cselect( selNames )
 
 
         assert( not resDf is None )
@@ -115,12 +106,9 @@ class TestTercen(unittest.TestCase):
     def test_select_all_rows(self):
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_3.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['']
 
-        resDf = tercenCtx.rselect( selNames )
+        resDf = self.context.rselect( selNames )
 
         assert( not resDf is None )
         assert(resDf.shape == targetDf.shape)
@@ -133,12 +121,9 @@ class TestTercen(unittest.TestCase):
     def test_select_rows(self):
         targetDf = pd.read_csv('tests/data/Test_Full_Projection_Table_3.csv')
 
-        tercenCtx = ctx.TercenContext( self.client, self.session, "9b611b90f412969d6f617f559f005bc6",
-                    "2ca54ff5-5b7f-44e9-870b-48facabc41ae")
-
         selNames = ['Rating.Effectiveness']
         targetDf = targetDf[selNames]
-        resDf = tercenCtx.rselect( selNames )
+        resDf = self.context.rselect( selNames )
 
 
         assert( not resDf is None )
