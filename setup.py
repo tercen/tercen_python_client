866b06b193fcb27324f18b2fb12329c30abefa32
Thiago Monteiro <tsmonteiro@gmail.com>
2022-11-23 14:22:19 -0300

    setup update

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen',
-    version='0.0.2',
+    version='0.0.1',
     packages=(
         find_packages(exclude="tests**") 
     ),
     install_requires=[
         'pandas >= 1.5, <1.6',
+        'numpy>=1.23,<1.24',
         'pytson @ git+https://github.com/tercen/pytson@1.5#egg=pytson'
     ],
 
