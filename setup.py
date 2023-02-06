e59e7aa5fceaf24feaaeba56a3a04dbd24a518a0
Thiago Monteiro <tsmonteiro@gmail.com>
2023-02-06 12:26:30 -0300

    More compatible requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen',
-    version='0.1.11',
+    version='0.1.12',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'numpy==1.23.4',
-        'pandas==1.5.1',
+        'numpy==1.19.5',
+        'pandas==1.1.5',
         'python-dateutil==2.8.2',
         'pytz==2022.6',
         'scipy==1.10.0',
