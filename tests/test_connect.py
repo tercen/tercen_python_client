9bb253e3a002dc4b4f2e6b16f46699c11335f50f
Thiago Monteiro <tsmonteiro@gmail.com>
2023-02-14 09:57:11 -0300

    test

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -5,7 +5,7 @@ from tercen.client.factory import TercenClient
 class TestTercen(unittest.TestCase):
     def test_connect(self):
         client = TercenClient("http://127.0.0.1:5402/")
-        session = client.userService.connect('test', 'test')
+        session = client.userService.connect('admin', 'admin')
         self.assertIsNotNone(session)
 
 
