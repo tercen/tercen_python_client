259ab191afa81c928bc436d336b6a63777fbbb7e
Thiago Monteiro <tsmonteiro@gmail.com>
2023-02-20 11:17:47 -0300

    wait conn

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,11 +1,21 @@
 import unittest
 from tercen.client.factory import TercenClient
-
+import time
 
 class TestTercen(unittest.TestCase):
     def test_connect(self):
-        client = TercenClient("http://172.42.0.42:5400/")
-        session = client.userService.connect('test', 'test')
+        fails = 0
+
+        while fails < 60:
+            try:
+                client = TercenClient("http://127.0.0.1:5402/")
+                session = client.userService.connect('test', 'test')
+                break
+            except:
+                fails = fails + 1
+                time.sleep(5)
+
+        
         self.assertIsNotNone(session)
 
 
