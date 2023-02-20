34f3746d3ec070722298da4dfcd34af69f8f8cc7
Thiago Monteiro <tsmonteiro@gmail.com>
2023-02-20 14:37:27 -0300

    update action

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,21 +1,12 @@
 import unittest
 from tercen.client.factory import TercenClient
-import time
+
 
 class TestTercen(unittest.TestCase):
     def test_connect(self):
-        fails = 0
-
-        while fails < 60:
-            try:
-                client = TercenClient("http://127.0.0.1:5402/")
-                session = client.userService.connect('test', 'test')
-                break
-            except:
-                fails = fails + 1
-                time.sleep(5)
-
         
+        client = TercenClient("http://127.0.0.1:5400/")
+        session = client.userService.connect('test', 'test')
         self.assertIsNotNone(session)
 
 
