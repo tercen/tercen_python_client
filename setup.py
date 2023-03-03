d90fba35cc48e1f500b96f773a754d78ea40a4f4
Thiago Monteiro <tsmonteiro@gmail.com>
2023-03-03 12:52:17 -0300

    client version+

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen',
-    version='0.3.3',
+    version='0.4.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy==1.23.*',
