801db900ea21d151d7206ca45715ec0721da5471
Thiago Monteiro <tsmonteiro@gmail.com>
2023-05-04 14:00:07 -0300

    version+

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen',
-    version='0.4.1',
+    version='0.5.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy==1.23.*',
