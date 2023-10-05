0e7414489324ab86b311c4787227e2481d52cddc
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-05 13:05:42 +0000

    correct pytson version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen_python_client',
-    version='0.7.17',
+    version='0.7.18',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy==1.24.*',
@@ -10,7 +10,7 @@ setup(
         'python-dateutil==2.8.*',
         'pytz==2022.6',
         'scipy==1.10.*',
-        'pytson @ git+https://github.com/tercen/pytson@1.7.6#egg=pytson',
+        'pytson @ git+https://github.com/tercen/pytson@1.7.7#egg=pytson',
         'polars==0.18.*',
         'pipreqs==0.4.*'
     ],
