3b7f342f6c61e5b8113b85c063eeb4eb9d201bca
Thiago Monteiro <tsmonteiro@gmail.com>
2024-01-30 14:32:18 +0000

    meta funcs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen_python_client',
-    version='0.9.4',
+    version='0.10.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy==1.22.*',
