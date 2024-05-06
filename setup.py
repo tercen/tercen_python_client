14a5fbb44b2ee0d078496ddf64adbf5591821118
Thiago Monteiro <tsmonteiro@gmail.com>
2024-05-06 15:07:13 +0000

    tagged ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tercen_python_client',
-    version='0.10.9',
+    version='0.10.10',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy==1.22.*',
