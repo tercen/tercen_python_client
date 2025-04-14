f03eefc9d79f4356be1d010ac851e87dc388a59e
Thiago <tsmonteiro@gmail.com>
2025-04-14 15:34:26 +0000

    Export params

--- tercen/util/export.py
+++ tercen/util/export.py
@@ -70,50 +70,21 @@ def export_to_project_as_csv(context: TercenContext, df,
     return (sch, df)
 
 
-# def export_to_project_as_csv(context, df, fname, projectId, folderId, user, workflowFolder=True):
-#     fname = "{}.csv".format(fname)
-
-#     context.log("Exporting {}: Writing temp file".format(fname))
-
-#     file = FileDocument()
-#     file.name = fname.split("/")[-1]
-#     file.acl.owner = user
-#     file.projectId = projectId
-
-#     file.folderId = folderId
-#     file.metadata.contentEncoding = "application/octet-stream"
-
-#     context.log("Exporting {}: Uploading".format(fname))
-#     fileDoc = context.context.client.fileService.uploadTable(file,
-#                                                              dataframe_to_table(df, values_as_list=True)[0].toJson())
-
-#     task = CSVTask()
-#     task.state = InitState()
-
-#     task.fileDocumentId = fileDoc.id
-#     task.projectId = projectId
-#     task.owner = user
-
-#     task = context.client.taskService.create(task)
-#     context.client.taskService.runTask(task.id)
-#     taskDone = context.client.taskService.waitDone(task.id)
-
-#     if workflowFolder == True:
-#         # Move to correct folder
-#         sch = context.client.tableSchemaService.get(taskDone.schemaId)
-#         sch.folderId = folderId
-#         context.client.tableSchemaService.update(sch)
-
-#     return df
-
-
 
 
 def export_df_to_project(context, data, fname,
                                  projectId, folderId, user, compression=1, fileExt="gz",
-                                 inplace=True, meta: list[Pair] = []):
+                                 inplace=True, meta: list[Pair] = [], timestamp = None, exportPrefix=""):
 
     fname = "{}.{}".format(fname, fileExt)
+
+    if not timestamp is None:
+        if not exportPrefix is None and exportPrefix != "":
+            exportPrefix += "_"
+        folderPath = get_folder_structure(folderId, context) + "/" + exportPrefix + timestamp
+        newFolder = context.context.client.folderService.getOrCreate(projectId, folderPath)
+        folderId = newFolder.id
+
 
     context.log("Exporting {}: Writing temp file".format(fname))
     with gzip.open(fname, 'wb', compresslevel=compression) as f:
